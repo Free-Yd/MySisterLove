9cdabd11e632422fa44ec5863b994bcd23fc8bcd
Free-Yd <2890337138@qq.com>
2017-04-12 21:32:36 +0800

    dsaddas

--- js/yd.js
+++ js/yd.js
@@ -1,8 +1,13 @@
-//触摸屏幕开启音乐（手机端不需自动播放）
+//开启音乐（手机端不需自动播放）
 
-//window.ontouchstart = function () {
-//    document.getElementById('music').play();
-//}
+//window.ontouchstart    触摸屏幕
+$('#start_img').click(function () {
+    document.getElementById('music').play();
+    $('.g_box').css('top','-100%');
+});
+setTimeout(function () {
+    $('#prompt_img').css('display', 'block')
+}, 5000);
 
 //判断屏幕旋转
 
@@ -27,7 +32,9 @@ addEventListener('load', function () {
 //切换图片与swiper
 
 var num = 0;
-var img = ['images/bg/bg.jpg', 'images/bg/bg1.jpg', 'images/bg/bg2.jpg', 'images/bg/bg3.jpg', 'images/bg/bg4.jpg', 'images/bg/bg5.jpg', 'images/bg/bg6.jpg', 'images/bg/bg7.jpg', 'images/bg/bg8.jpg'];
+var img = ['images/bg/bg.jpg', 'images/bg/bg1.jpg', 'images/bg/bg2.jpg', 'images/bg/bg3.jpg', 'images/bg/bg4.jpg'];
+var page4_off = true;
+var page5_off = true;
 
 //1>初始赋予背景图  第一页效果
 function Bg_img() {
@@ -54,6 +61,14 @@ var mySwiper = new Swiper('.swiper-container', {
         if (num == 2 && $('#demo3 img').length == 0) {
             Page3();
         }
+        if (num == 3 && page4_off) {
+            page4_off = false;
+            Page4();
+        }
+        if (num == 4 && page5_off) {
+            page5_off = false;
+            Page5();
+        }
     },
     onSlidePrevStart: function (swiper) {
         $('.g_bg').removeClass('g_bg1');
@@ -66,7 +81,7 @@ var mySwiper = new Swiper('.swiper-container', {
 });
 //2>第二页效果
 function Page2() {
-    var word2 = ['— —', '在', '偌', '大', '个', '城', '市', '中', '，', '让', '我', '遇', '到', '你', '，', '从', '这', '一', '刻', '起', '，', '我', '就', '知', '道', '，', '你', '就', '是', '我', '的', '命', '中', '注', '定', '~'];
+    var word2 = ['— —', '偌', '大', '个', '城', '市', '，', '让', '我', '遇', '见', '你', '，', '从', '那', '一', '刻', '起', '，', '我', '就', '知', '道', '，', '你', '就', '是', '我', '的', '命', '中', '注', '定', '~'];
     var time2 = 0;
     for (var i = 0; i < word2.length; i++) {
         $('#word').append('<span class="fl animated bounceInDown" style="-webkit-animation-delay:' + 0.4 * i + 's;animation-delay:' + 0.4 * i + 's;">' + word2[i] + '</span>');
@@ -81,7 +96,75 @@ function Page3() {
     for (var i = 0; i < 30; i++) {
         $('#demo3').append('<img src="' + img3[parseInt(Math.random() * 4)] + '" alt="" class="imgmove" style=" left:' + parseInt(Math.random() * 90) + '%; top:' + parseInt(Math.random() * 90) + '%; animation-name:' + moveName[parseInt(Math.random() * 9)] + ';-webkit-animation-name:' + moveName[parseInt(Math.random() * 9)] + '; animation-delay:' + (Math.random() * 5).toFixed(1) + 's; " >')
     }
-    $('#demo3_img3').css({transitionDelay:'2s',transform:'scale(10) rotate(-5deg)',opacity:'1'});
-    $('#demo3_img2').css({transitionDelay:'4s',transform:'scale(10) rotate(12deg)',opacity:'1'});
-    $('#demo3_img1').css({transitionDelay:'6s',transform:'scale(10) rotate(-14deg)',opacity:'1'});
+    $('#demo3_img3').css({
+        transitionDelay: '2s',
+        transform: 'scale(10) rotate(-5deg)',
+        opacity: '1'
+    });
+    $('#demo3_img2').css({
+        transitionDelay: '4s',
+        transform: 'scale(10) rotate(12deg)',
+        opacity: '1'
+    });
+    $('#demo3_img1').css({
+        transitionDelay: '6s',
+        transform: 'scale(10) rotate(-14deg)',
+        opacity: '1'
+    });
 }
+//4>第四页效果
+
+function CountDown() {
+    var timer = null;
+    var oTimeBox = document.getElementById('timeBox');
+    var will = new Date();
+    will.setFullYear(2017, 04, 07);
+    will.setHours(10, 0, 0);
+
+    function time() {
+        var now = new Date();
+        var time_miao = (will.getTime() - now.getTime()) / 1000;
+        //---------------------------------------得出总秒，要除1000；他最开始单位是毫秒
+        var date = Math.floor(time_miao / (60 * 60 * 24));
+        var h_miao = time_miao % (60 * 60 * 24);
+        //----------------------------------------得出几天，且剩余几秒
+        var hours = Math.floor(h_miao / 3600);
+        var m_miao = h_miao % 3600;
+        //---------------------------------------得出几小时，且剩余几秒
+        var sec = Math.floor(m_miao / 60);
+        var s_miao = Math.floor(m_miao % 60);
+        //----------------------------------------得出几分，且剩余几秒
+        oTimeBox
+            .innerHTML = '~~距婚礼还有' +
+            toDouble(date) + '天' +
+            toDouble(hours) + '时' +
+            toDouble(sec) + '分' +
+            toDouble(s_miao) + '秒~~';
+    }
+    timer = setInterval(time, 1000);
+    //------------------------------------00:00:00模式------
+    function toDouble(n) {
+        if (n < 10) {
+            return '0' + n;
+        } else {
+            return '' + n;
+        }
+    }
+};
+CountDown();
+
+function Page4() {
+    $('#demo4').css('transform', 'scale(3) translateY(-30%)');
+}
+//5>第五页效果
+function Page5() {
+    $('#demo5').css('display', 'block');
+    for (var i = 0; i < $('#demo5 li').length; i++) {
+        $('#demo5 li').eq(i).addClass('animated bounceInLeft').css({
+            webkitAnimationDelay: (i + 1) + 's',
+            animationDelay: (i + 1) + 's',
+            webkitAnimationDuration: '1.2s',
+            animationDuration: '1.2s'
+        });
+    }
+};
